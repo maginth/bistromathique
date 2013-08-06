33fd049bacbc4f06b23a0a99862bc513a8608387
Mathieu GUININ <mguinin@e3r9p5.42.fr>
2013-08-06 15:09:09 +0200

    types.h

--- types.h
+++ types.h
@@ -6,24 +6,26 @@
 /*   By: mguinin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/08/05 19:46:15 by mguinin           #+#    #+#             */
-/*   Updated: 2013/08/05 20:14:18 by mguinin          ###   ########.fr       */
+/*   Updated: 2013/08/06 14:59:06 by mguinin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef TYPE_H
-# define TYPE_H
+#ifndef TYPES_H
+# define TYPES_H
 typedef		unsigned long	ulong;
 typedef		unsigned int	uint;
 typedef		struct		s_big_long
 {
+	int					sgn;
 	long				*data;
 	int					len;
 }						t_big_long
 
 typedef		struct		s_big_char
 {
-	long				*data;
+	int					sgn;
+	char				*data;
 	int					len;
 }						t_big_char
-#endif /* TYPE_H */
+#endif /* TYPES_H */
 
